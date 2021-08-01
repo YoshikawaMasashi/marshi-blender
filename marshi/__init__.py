900c310884f074a7f3b8937dfdea9346629a8175
DESKTOP-RFPJ7SV\earne <earnest.marshi@gmail.com>
2021-08-01 23:28:22 +0900

    empty

---
+++ marshi/__init__.py
@@ -0,0 +1,23 @@
+import bpy
+
+bl_info = {
+    "name": "marshi",
+    "author": "marshi",
+    "description": "marshi blender addon",
+    "version": (0, 0, 1),
+    "blender": (2, 90, 0),
+    "location": "",
+    "url": "",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": ""
+}
+
+def register():
+    pass
+
+def unregister():
+    pass
+
+if __name__ == "__main__":
+    register()
